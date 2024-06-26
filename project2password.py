022fb45a9031260f881f9f9d008bdc570e4e51ad
Anam Shaikh <147983719+Anam123s@users.noreply.github.com>
2024-06-26 16:26:55 +0530

    Add files via upload

---
+++ project2password.py
@@ -0,0 +1,31 @@
+import random
+print("Welcome to password generator 😜")
+letters=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+
+symbols=['!','@','#','$','%','^','&','*','(',')','+','-','*','/']
+
+numbers=['1','2','3','4','5','6','7','8','9','0']
+
+no_letters=int(input("How many letters do you want in your password: "))
+no_symbols=int(input("How many symbols do you want: "))
+no_numbers=int(input("How many numbers do you want: "))
+
+password=[]
+for i in range(1,no_letters+1):
+    fun=random.choice(letters)
+    password+=fun
+
+
+for i in range(1,no_symbols+1):
+    fun=random.choice(symbols)
+    password+=fun
+
+
+for i in range(1,no_numbers+1):
+    fun=random.choice(numbers)
+    password+=fun
+random.shuffle(password)
+new_pass=""
+for i in password:
+    new_pass+=i
+print(new_pass)
